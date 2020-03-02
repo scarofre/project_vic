d1912d181e3ab38dc358f6f382dae907a147548c
oscar <oscar.frelot@supelec.fr>
2020-03-02 14:12:31 +0100

    Modification model pour avoir different predicator

--- data_preprocess.py
+++ data_preprocess.py
@@ -2,11 +2,14 @@ import cv2
 import numpy as np
 import pandas as pd
 import skimage
+import dlib
 
 from skimage.feature import hog
 from skimage import data, exposure
 
 from sklearn.preprocessing import OneHotEncoder
+
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
 
 class dataFormatter:
@@ -23,8 +26,8 @@ class dataFormatter:
         hog_images = []
 
         for image in self.images:
-            features, hog_image = hog(image, orientations=8, pixels_per_cell=(16, 16),
-                                      cells_per_block=(1, 1), visualise=True)
+            features, hog_image = hog(image, orientations=8, pixels_per_cell=(5, 5),
+                                      cells_per_block=(2, 2), visualize=True)
             hog_features.append(features)
             hog_images.append(hog_image)
 
@@ -35,8 +38,13 @@ class dataFormatter:
     ## compute landmarks features
 
     def compute_landmarks(self):
-        # TODO
-        return
+        landmarks = []
+        for image in self.images:
+            face_rects = [dlib.rectangle(left=1, top=1, right=47, bottom=47)]
+            face_landmarks = np.matrix([[p.x, p.y] for p in predictor(image, face_rects[0]).parts()])
+            landmarks.append(face_landmarks)
+        self.data['landmarks'] = landmarks
+        return landmarks
 
     def process_target(self):
         enc = OneHotEncoder(handle_unknown='ignore', sparse=False)
